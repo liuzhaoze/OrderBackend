2c242ee5336253b7a4969cb456432d4cedd70e21
liuzhaoze <1045954863@qq.com>
2025-05-23 09:18:02 +0800

    实现基于内存的 stock 数据库

---
+++ stock/infrastructure/database/memory.go
@@ -0,0 +1,88 @@
+package database
+
+import (
+	"context"
+	"stock/domain"
+	"sync"
+)
+
+type MemoryDatabase struct {
+	lock *sync.RWMutex
+	db   []*domain.Item
+}
+
+func NewMemoryDatabase() *MemoryDatabase {
+	return &MemoryDatabase{
+		lock: &sync.RWMutex{},
+		db: []*domain.Item{
+			{ItemID: "test_item_1", Name: "item1", Quantity: 100, PriceID: "price_test_item_1"},
+			{ItemID: "test_item_2", Name: "item2", Quantity: 200, PriceID: "price_test_item_2"},
+		},
+	}
+}
+
+func (m *MemoryDatabase) Get(ctx context.Context, itemIDs []string) ([]*domain.Item, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	var (
+		result  = make([]*domain.Item, 0, len(itemIDs))
+		missing = make([]string, 0)
+	)
+
+	for _, target := range itemIDs {
+		isExist := false
+		for _, existing := range m.db {
+			if existing.ItemID == target {
+				isExist = true
+				result = append(result, domain.NewItem(existing.ItemID, existing.Name, existing.Quantity, existing.PriceID))
+			}
+		}
+		if !isExist {
+			missing = append(missing, target)
+		}
+	}
+
+	if len(missing) > 0 {
+		return nil, &domain.NotFoundError{MissingItemIDs: missing}
+	}
+	return result, nil
+}
+
+// Update 首先找到 items 在数据库中对应的所有对象，然后将这些对象按照 updateFunc 的逻辑进行修改，最后返回修改后的对象们
+func (m *MemoryDatabase) Update(ctx context.Context, items []*domain.Item, updateFunc func(context.Context, []*domain.Item) ([]*domain.Item, error)) ([]*domain.Item, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	var (
+		result  = make([]*domain.Item, 0, len(items))
+		missing = make([]string, 0)
+	)
+
+	for _, target := range items {
+		isExist := false
+		for _, existing := range m.db {
+			if existing.ItemID == target.ItemID {
+				isExist = true
+				result = append(result, existing)
+			}
+		}
+		if !isExist {
+			missing = append(missing, target.ItemID)
+		}
+	}
+
+	if len(missing) > 0 {
+		return nil, &domain.NotFoundError{MissingItemIDs: missing}
+	}
+
+	if updatedItems, err := updateFunc(ctx, result); err != nil {
+		return nil, err
+	} else {
+		newUpdatedItems := make([]*domain.Item, 0, len(updatedItems))
+		for _, item := range updatedItems {
+			newUpdatedItems = append(newUpdatedItems, domain.NewItem(item.ItemID, item.Name, item.Quantity, item.PriceID))
+		}
+		return newUpdatedItems, nil
+	}
+}
