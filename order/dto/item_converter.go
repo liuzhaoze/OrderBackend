04b968fa4b5ede46b45fae6f0b4d67d6764e4b1e
liuzhaoze <1045954863@qq.com>
2025-05-26 13:55:37 +0800

    扩充 order 模块的 dto 转换

--- order/dto/item_converter.go
+++ order/dto/item_converter.go
@@ -1,6 +1,7 @@
 package dto
 
 import (
+	"common/protobuf/orderpb"
 	"common/protobuf/stockpb"
 	"order/domain"
 )
@@ -15,10 +16,34 @@ func (c *ItemConverter) FromStockGrpc(item *stockpb.Item) *domain.Item {
 	return &domain.Item{ItemID: item.ItemID, Name: item.Name, Quantity: item.Quantity, PriceID: item.PriceID}
 }
 
+func (c *ItemConverter) FromOrderGrpc(item *orderpb.Item) *domain.Item {
+	return &domain.Item{ItemID: item.ItemID, Name: item.Name, Quantity: item.Quantity, PriceID: item.PriceID}
+}
+
 func (c *ItemConverter) FromStockGrpcBatch(items []*stockpb.Item) []*domain.Item {
 	result := make([]*domain.Item, len(items))
 	for i, item := range items {
 		result[i] = c.FromStockGrpc(item)
+	}
+	return result
+}
+
+func (c *ItemConverter) FromOrderGrpcBatch(items []*orderpb.Item) []*domain.Item {
+	result := make([]*domain.Item, len(items))
+	for i, item := range items {
+		result[i] = c.FromOrderGrpc(item)
+	}
+	return result
+}
+
+func (c *ItemConverter) ToOrderGrpc(item *domain.Item) *orderpb.Item {
+	return &orderpb.Item{ItemID: item.ItemID, Name: item.Name, Quantity: item.Quantity, PriceID: item.PriceID}
+}
+
+func (c *ItemConverter) ToOrderGrpcBatch(items []*domain.Item) []*orderpb.Item {
+	result := make([]*orderpb.Item, len(items))
+	for i, item := range items {
+		result[i] = c.ToOrderGrpc(item)
 	}
 	return result
 }
---
+++ order/dto/order_converter.go
@@ -0,0 +1,34 @@
+package dto
+
+import (
+	"common/protobuf/orderpb"
+	"order/domain"
+)
+
+type OrderConverter struct{}
+
+func NewOrderConverter() *OrderConverter {
+	return &OrderConverter{}
+}
+
+func (c *OrderConverter) FromOrderGrpc(order *orderpb.Order) *domain.Order {
+	status, _ := NewOrderStatusConverter().FromOrderGrpc(order.Status)
+	return &domain.Order{
+		OrderID:     order.OrderID,
+		CustomerID:  order.CustomerID,
+		Items:       NewItemConverter().FromOrderGrpcBatch(order.Items),
+		Status:      status,
+		PaymentLink: order.PaymentLink,
+	}
+}
+
+func (c *OrderConverter) ToOrderGrpc(order *domain.Order) *orderpb.Order {
+	status, _ := NewOrderStatusConverter().ToOrderGrpc(order.Status)
+	return &orderpb.Order{
+		OrderID:     order.OrderID,
+		CustomerID:  order.CustomerID,
+		Items:       NewItemConverter().ToOrderGrpcBatch(order.Items),
+		Status:      status,
+		PaymentLink: order.PaymentLink,
+	}
+}
