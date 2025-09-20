40a59ace5102e802471e49c5829584e6ccd6909f
liuzhaoze <1045954863@qq.com>
2025-09-20 10:54:40 +0800

    使用悲观锁解决并发更新 MySQL 导致的资源竞争

--- stock/infrastructure/database/mysql.go
+++ stock/infrastructure/database/mysql.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"stock/domain"
 	"time"
 )
@@ -50,19 +51,37 @@ func (m *MySQLDatabase) Update(ctx context.Context, items []*domain.Item, update
 	ctx, span := tracing.StartSpan(ctx, "Stock Repository: update")
 	defer span.End()
 
-	var updatedItems []*domain.Item
-	err := m.db.Transaction(func(tx *gorm.DB) (err error) {
+	return m.updatePessimistic(ctx, items, updateFunc)
+}
+
+func (m *MySQLDatabase) updatePessimistic(ctx context.Context, items []*domain.Item, updateFunc func(context.Context, []*domain.Item) ([]*domain.Item, error)) ([]*domain.Item, error) {
+	var (
+		updatedItems []*domain.Item
+		err          error
+	)
+
+	err = m.db.Transaction(func(tx *gorm.DB) (err error) {
 		itemIDs := make([]string, len(items))
 		for i, item := range items {
 			itemIDs[i] = item.ItemID
 		}
 
 		var result []*itemModel
-		tx.Where("item_id IN ?", itemIDs).Find(&result)
-		updatedItems, err = updateFunc(ctx, m.unmarshalItemBatch(result))
+		if err = tx.Clauses(clause.Locking{Strength: clause.LockingStrengthUpdate}).
+			Where("item_id IN ?", itemIDs).Find(&result).Error; err != nil {
+			return
+		}
+
+		if updatedItems, err = updateFunc(ctx, m.unmarshalItemBatch(result)); err != nil {
+			return
+		}
 
 		for _, item := range m.marshalItemBatch(updatedItems) {
-			tx.Where("item_id = ?", item.ItemID).Updates(item)
+			if err = tx.Where("item_id = ?", item.ItemID).Select("quantity").Updates(item).Error; err != nil {
+				// 如果不添加 .Select() ，gorm 会只更新非零值字段，无法将某些字段更新为零值
+				// 参见 https://gorm.io/docs/update.html#Updates-Changed-Only
+				return
+			}
 		}
 
 		return nil
